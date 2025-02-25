bc661d784da785b483c0d722a8fb64dc03220ab4
Yoann Lenoel <ylenoel@paul-f4Ar7s4.clusters.42paris.fr>
2025-02-25 18:18:05 +0100

    Ex02 en corus

---
+++ ex02/includes/Fixed.hpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylenoel <ylenoel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 11:03:00 by ylenoel           #+#    #+#             */
+/*   Updated: 2025/02/25 17:38:07 by ylenoel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed{
+	
+	private :
+	
+	int value;
+	static const int nb_Bits = 8;
+	
+	public :
+
+	Fixed();
+	Fixed(const int value);
+	Fixed(const float value); 
+	~Fixed();
+	Fixed(const Fixed& other); // Constructeur de copie
+	Fixed& 	operator=(const Fixed& other); // Opérateur d'affectation surchargée
+	
+	/* Surcharge d'opérateurs arythmétiques */
+	Fixed&	operator+(const Fixed& other) const;
+	Fixed&	operator-(const Fixed& other) const;
+	Fixed& 	operator*(const Fixed& other) const;
+	Fixed&	operator/(const Fixed& other) const;
+
+	/* Surcharge d'opérateur pré incrémentation */ 
+	Fixed&	operator++();
+	Fixed&	operator--();
+	/* Surcharge d'opérateur pré incrémentation */ 
+	Fixed	operator++(int);
+	Fixed	operator--(int);
+
+	/* Surcharge d'opérateur de comparaison */
+
+	bool operator==(const Fixed& other) const;
+	bool operator!=(const Fixed& other) const;
+	bool operator<(const Fixed& other) const;
+	bool operator>(const Fixed& other) const;
+	bool operator<=(const Fixed& other) const;
+	bool operator>=(const Fixed& other) const;
+	
+	
+	
+	int 	getRawBits(void) const;
+	void 	setRawBits(int const raw);
+	float 	toFloat(void) const;
+	int		toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
+
+#endif
---
+++ ex02/src/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylenoel <ylenoel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 15:06:13 by ylenoel           #+#    #+#             */
+/*   Updated: 2025/02/25 11:16:56 by ylenoel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+#include <iostream>	
+
+int main(void)
+{
+	Fixed		a;
+	Fixed const	b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	
+	std:: << b << std::endl;
+	
+	std::cout << Fixed::max(a, b) << std::endl;
+
+	return (0);
+}
